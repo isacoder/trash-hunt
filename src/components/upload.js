169c7cd50003465dc2475db34a19ab4a6ddd994a
yukinoda <yukinoda08@gmail.com>
2019-02-17 08:19:16 +0900

    enabled auto-tagging

--- src/components/upload.js
+++ src/components/upload.js
@@ -5,12 +5,51 @@ import '../assets/styles/upload.css';
 import uploadButton from '../assets/images/upload-button.png';
 
 class Upload extends Component {
-  uploadWidget() {
-    window.cloudinary.openUploadWidget({ cloud_name: 'yukinoda', upload_preset: 'trash-hunt-items', tags: ['gallery-item','my-item']},
+  componentDidMount = () => {
+    this.widget = window.cloudinary.createUploadWidget({
+      cloud_name: 'yukinoda', upload_preset: 'trash-hunt-items', 
+      tags: ['my-item', 'gallery-item'], 
+      sources: [
+        "local"
+      ],
+      styles: {
+        palette: {
+          window: "#464040",
+          sourceBg: "#292222",
+          windowBorder: "#c7a49f",
+          tabIcon: "#cc6600",
+          inactiveTabIcon: "#E8D5BB",
+          menuIcons: "#ebe5db",
+          link: "#ffb107",
+          action: "#ffcc00",
+          inProgress: "#99cccc",
+          complete: "#78b3b4",
+          error: "#ff6666",
+          textDark: "#4C2F1A",
+          textLight: "#D8CFCF"
+        },
+        fonts: {
+          default: null,
+          "'Merriweather', serif": {
+            url: "https://fonts.googleapis.com/css?family=Merriweather",
+            active: true
+          }
+        }
+      }
+    },
       (error, result) => {
         console.log(result);
-      });
+        if (result && result.event === "success") {
+          this.widget.close()
+        }
+      }
+    );
   }
+
+  uploadWidget = () => {
+    this.widget.open()
+  }
+
   render() {
     return (
       <div>
